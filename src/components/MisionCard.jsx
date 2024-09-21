c1af6c9e3aacfe8cd66794590b5e14bc9b6d8e5f
Ronaldo Acevedo Ojeda <ronaldoa.ojeda@gmail.com>
2024-09-21 10:15:44 -0600

    Primer Historia

--- src/components/MisionCard.jsx
+++ src/components/MisionCard.jsx
@@ -193,6 +193,123 @@ export default function MisionCard({ imagenG, imagenCH, titulo, descripcion, act
             </div>
           </div>
 }
+{(screen == 2) &&
+          <div className="modal-box w-11/12 max-w-5xl flex flex-row sm:flex-col">
+          
+            <div>
+              <img src={images.mascotaConPan} className="w-48" />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Hace 1400 años</h3>
+              <p className="py-4">
+                Este es uno de los pane mas rico que hay méxico, que se acostumbre comer en el mes de Octubre o Noviembre{" "}
+                <br />
+                ¿Pero porque en este mes exactamente y que es lo que representa?
+                <br />
+                Bueno <b>comencemos</b> con esta historia
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button, it will close the modal */}
+                  <button className="btn" onClick={() => {setScreen(3)}}>Next</button>
+                  
+                </form>
+              </div>
+            </div>
+          </div>
+}
+{(screen == 3) &&
+          <div className="modal-box w-11/12 max-w-5xl flex flex-row sm:flex-col">
+          
+            <div>
+              <img src={images.mascotaSanJuditas} className="w-48" />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Reliquias</h3>
+              <p className="py-4">
+                En el siglo XI los restos de los santos de la iglesia Catolica adquirieron popularidad ya 
+                que se consideraban poderosos, y era un gran negocio de los saqueadores de las catacumbas 
+              {" "}
+                <br />
+                El plan de la iglesia era tenerlo guardados bajo la reliquia mayor, el cuerpo de Cristo
+                <br />
+                En ese tiempo miles de persona anononimas murieron por difundir las enseñanzas de Cristo, pero
+                a muchos nadie los recordaba por lo que los papas y abares decidieron crear celebración en su honra,
+                la celebración de <b>Todos los Santos</b>
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button, it will close the modal */}
+                  <button className="btn" onClick={() => {setScreen(4)}}>Next</button>
+                  
+                </form>
+              </div>
+            </div>
+          </div>
+}
+{(screen == 4) &&
+          <div className="modal-box w-11/12 max-w-5xl flex flex-row sm:flex-col">
+          
+            <div className="mr-10"> 
+              <img src={images.mascotaConAltar} className="w-80  " />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Todos los santos</h3>
+              <p className="py-4">
+                La festividad consistia en que las familias con difuntos acudian a la iglesia con alimentos que posteriormente serian bendecidos
+                
+              {" "}
+                <br />
+                Dentro de estos alimentos, muchas personas empezaron a hacer dulces para llevar a la iglesia,
+                estos dulces y/o panes eran peculiares ya que tenian forma de las reliquias sagradas
+                osea en forma de huesos.
+                <br />
+                Despues de que la comida era bendecida las familias ponian estos alimentos en algo que se conocia como la mesa del muerto, el antepasado de
+                lo que hoy se conoce como altar
+                
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button, it will close the modal */}
+                  <button className="btn" onClick={() => {setScreen(5)}}>Next</button>
+                  
+                </form>
+              </div>
+            </div>
+          </div>
+}
+
+{(screen == 5) &&
+          <div className="modal-box w-11/12 max-w-5xl flex flex-row sm:flex-col">
+          
+            <div className="mr-10"> 
+              <img src={images.mascotaConPan} className="w-80  " />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Pan de muerto</h3>
+              <p className="py-4">
+                Por cierto.... notas algo en los panes de muerto tradicionales de mexico?
+              {" "}
+                <br />
+                Así es, aquellos dulces de huesos se mantienen hasta el dia de hoy.
+                <br />
+                Tiempo despues esta tradición se trajo a méxico por los conquistadores, 
+                se mezclo con tradiciones locales y quedó la misma esencia
+                <br/>
+                <br/>
+                ¿Listo para tu primer misión? Vayamos a probar un buen pan de muerto  
+                
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button, it will close the modal */}
+                  <button className="btn" >Vamos</button>
+                  
+                </form>
+              </div>
+            </div>
+          </div>
+}
         </dialog>
 
       
