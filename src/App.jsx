8bb876efcac339a7db5c9adff3fa6550a6163ec5
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-10-20 02:20:06 -0600

    limpiando el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,17 @@
-import { icon } from "leaflet";
-import ButtonNeon from "./components/ButtonNeon";
-import Navbar from "./components/Navbar";
-import icons from "./constants/icons";
-import Advance from "./components/Advance";
-import MisionCard from "./components/MisionCard";
-import Sidebar from "./components/Sidebar";
 import { useEffect, useState } from "react";
-import Map from "./Map";
-import DrawerIA from "./components/DrawerIA";
-import images from "./constants/images";
-import Favicon from "./components/Favicon";
-import Tienda from "./Tienda";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Onboarding from "./pages/onboarding";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 export default function App() {
-  const [screen, setScreen] = useState("home");
-  const [init, setInit]  = useState(false);
-
-  useEffect(() => {
-    // Mostrar el modal al cargar la página
-    document.getElementById("bienvenida").showModal()
-  }, []);
-
-  const muertos = ["Pan de muerto","Panteon", "Sempasuchil", "Recorre", "Gastronomia"]
-  const traji = ["Experiencia", "Taller", "Guia", "Conoce", "Gastronomia"]
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
-
-    window.addEventListener("resize", handleResize);
-
-    // Limpia el evento cuando el componente se desmonta
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return (
-    <>
-      <Navbar setScreen={setScreen} />
-      <div className="flex">
-        <Sidebar setScreen={setScreen} />
-
-        <div className=" w-[90%] h-screen pt-20  sm:w-[100%] align-middle relative">
-          
-
-          {screen == "home" && (
-            <div className="h-full flex flex-col">
-            <Advance visible={init}/>
-            <div className="w-full sm:w-screen   p-4 overflow-y-scroll">
-              <div className="flex flex-row sm:flex-col justify-between">
-                <h2 className="text-xl font-bold mb-4">Lo mas top</h2>
-                <select className="select select-bordered w-full max-w-xs">
-                  <option disabled selected>
-                    O Elige un Estado
-                  </option>
-                  <option>Oaxaca</option>
-                    <option>Aguascalientes</option>
-                    <option>Baja California</option>
-                    <option>Baja California Sur</option>
-                    <option>Campeche</option>
-                    <option>Chiapas</option>
-                    <option>Chihuahua</option>
-                    <option>Ciudad de México</option>
-                    <option>Coahuila</option>
-                    <option>Colima</option>
-                    <option>Durango</option>
-                    <option>Guanajuato</option>
-                    <option>Guerrero</option>
-                    <option>Hidalgo</option>
-                    <option>Jalisco</option>
-                    <option>Estado de México</option>
-                    <option>Michoacán</option>
-                    <option>Morelos</option>
-                    <option>Nayarit</option>
-                    <option>Nuevo León</option>
-                    <option>Oaxaca</option>
-                    <option>Puebla</option>
-                    <option>Querétaro</option>
-                    <option>Quintana Roo</option>
-                    <option>San Luis Potosí</option>
-                    <option>Sinaloa</option>
-                    <option>Sonora</option>
-                    <option>Tabasco</option>
-                    <option>Tamaulipas</option>
-                    <option>Tlaxcala</option>
-                    <option>Veracruz</option>
-                    <option>Yucatán</option>
-                    <option>Zacatecas</option>
-                </select>
-              </div>
-
-              <p className="mb-2">
-                Hola hemos visto que estas en CDMX te presento lo mas revelante
-                que puedes encontrar aquí
-              </p>
-              {!init &&
-              <MisionCard imagenG="https://media.admagazine.com/photos/618a5eb3a9f7fab6f0622baa/1:1/w_2000,h_2000,c_limit/96749.jpg" imagenCH={images.muertos} titulo="Dia de muertos" descripcion="Una de las festividades con mas tradicion de todo méxico"
-              actividades={muertos} setInit={setInit} /> }
-              <MisionCard  imagenG="https://trajinerasvipxochimilco.com/wp-content/uploads/2023/08/imagen-principal.jpg" imagenCH={images.xochimilco} titulo="Las trajineras" descripcion=""
-              actividades={traji}/>
-              
-            </div>
-            </div>
-          )}
-          {screen == "map" && 
-            
-            <Map />
-           
-          }
-          {
-            screen == "tienda" &&
-            <Tienda/>
-          }
-          <DrawerIA/>
-          {windowWidth < 768 &&
-          <Favicon setScreen={setScreen} screen={screen}/>}
-        </div>
-      </div>
-      <dialog id="bienvenida" className="modal">
-        <div className="modal-box w-11/12 max-w-5xl flex flex-row sm:flex-col">
-          <div>
-            <img src={images.mascota02} className="w-48"/>
-          </div>
-          <div>
-            <h3 className="font-bold text-lg">Hola!</h3>
-            <p className="py-4">Te doy la bienvenida a nuestra plataforma.. 
-
-              ¿Estas listo para conocer la gran diversidad de tradiciones y cultura que tiene México?
-              <br/>
-              Cumple con cada uno de los retos, explora nuevos lugares y sobre todo diviertete que al final de cada aventura tenemos grandes
-              recompensas para ti 
-            </p>
-            <div className="modal-action">
-              <form method="dialog">
-                {/* if there is a button, it will close the modal */}
-                <button className="btn">Adelante</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </dialog>
-    </>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Signup />} />
+				<Route path="/onboarding" element={<Onboarding />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</Router>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,19 @@
-import { comma } from 'postcss/lib/list';
+import { comma } from "postcss/lib/list";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    screens: {
-			"custom-lg": "1024px",
-			// => @media (max-width: 1535px) { ... }
-
-			xl: { max: "1279px" },
-			// => @media (max-width: 1279px) { ... }
-
-			lg: { max: "1023px" },
-			// => @media (max-width: 1023px) { ... }
-
-			md: { max: "767px" },
-			// => @media (max-width: 767px) { ... }
-
-			sm: { max: "639px" },
-			// => @media (max-width: 639px) { ... }
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		screens: {},
+		extend: {
+			fontFamily: {
+				comicfont: ["Comic", "sans-serif"],
+				Montse: ["Montserrat", "sans-serif"],
+			},
 		},
-    extend: {
-      fontFamily:{
-        comicfont: ["Comic", "sans-serif"],
-        Montse: ["Montserrat","sans-serif"]
-      }
-    },
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: ["light", "dark", "pastel"],
+	},
+};
